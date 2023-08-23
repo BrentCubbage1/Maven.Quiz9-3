f9445730aa29f65ecce81eec24ed4f1a2f3d63ce
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-23 14:34:01 -0400

    Isogram???

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,24 +1,83 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
+
+    public static boolean isEven(int i){
+        return i % 2 == 0;
+    }
+
     public static Character getMiddleCharacter(String string) {
-        return null;
+        Character middleChar;
+        Integer lastIndex = string.length() - 1;
+        if( isEven(lastIndex)){
+            middleChar = string.charAt(lastIndex / 2);
+        } else {
+            middleChar = string.charAt((lastIndex / 2) - 1);
+        }
+        return middleChar;
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        //The most hideous code in my life
+        Integer middleIndex;
+        Integer lastIndex = str.length() - 1;
+        if( isEven(lastIndex)){
+            middleIndex = lastIndex / 2;
+        } else {
+            middleIndex = (lastIndex / 2) - 1;
+        }
+       char[] characters = str.toCharArray();
+        characters[middleIndex] = Character.toUpperCase(characters[middleIndex]);
+        String result = "";
+        for (char character : characters){
+            result += character;
+        }
+
+        return result;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        //The most hideous code in my life pt. 2
+        Integer middleIndex;
+        Integer lastIndex = str.length() - 1;
+        if( isEven(lastIndex)){
+            middleIndex = lastIndex / 2;
+        } else {
+            middleIndex = (lastIndex / 2) - 1;
+        }
+        char[] characters = str.toCharArray();
+        characters[middleIndex] = Character.toLowerCase(characters[middleIndex]);
+        String result = "";
+        for (char character : characters){
+            result += character;
+        }
+
+        return result;
     }
 
     /* isogram (plural isograms) A word or phrase in which each letter occurs the same number of times. read the tests. */
     public static Boolean isIsogram(String str) {
-        return null;
+        Boolean isogram = true;
+        int count = 0;
+        //Here - Works
+        //XXX - Doesn't work
+        for (int i = 0; i < str.length(); i++) {
+            int temp = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    temp++;
+                }
+                if (temp > 1) {
+                    isogram = false;
+                }
+            }
+        }
+        return isogram;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
