3445e230adeaa880269622e23fee578e0cc57639
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-23 15:00:19 -0400

    complete 88/88 tests

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,43 @@
 package rocks.zipcode.quiz4.arrays;
 
+import rocks.zipcode.quiz4.fundamentals.Calculator;
+import rocks.zipcode.quiz4.fundamentals.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+
+        return StringUtils.isEven(values.length) ? values[(values.length / 2) - 1] : values[values.length / 2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        List<String> result = new ArrayList<>();
+
+        for (String value : values){
+            if (!value.equals(getMiddleElement(values))){
+                result.add(value);
+            }
+        }
+
+        return result.toArray(new String[0]);
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < values.length - 1; i++){
+            result.add(values[i]);
+        }
+
+        return result.toArray(new String[0]);
     }
 }
